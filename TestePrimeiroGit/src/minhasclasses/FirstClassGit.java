aeb5f8157f0007a637762d6894e74aec62fac4fe
etorres74 <edmilson@L-Debian>
2019-12-10 23:33:00 -0200

    mais um comite no dia.. agora tem até junit instalado na bagaça. A próxima missão é tentar -- abrir um outro repositório e trabalhar com ele para ver no que vai dar.. mas este ainda pode ser utilizado para realização de testes.. muitos testes sem eira nem beira nesta bagaça

--- TestePrimeiroGit/src/minhasclasses/FirstClassGit.java
+++ TestePrimeiroGit/src/minhasclasses/FirstClassGit.java
@@ -1,5 +1,7 @@
 package minhasclasses;
 
+import org.junit.Test;
+
 public class FirstClassGit {
 
 	public static void main(String[] args) {
@@ -7,4 +9,34 @@ public class FirstClassGit {
    System.out.println("Este foi o primeiro projeto no Git, Agora eu estou melhorando!");
 	}
 
-}
+	
+	@Test
+	
+	public void teste() {
+		
+		System.out.println("Eita Coisa boa... tem muito o que fazer ainda");
+	}
+	
+	
+	@Test  // Aplicar a técnica de cobertura por sentença para testes unitários 
+	
+	public void calculoBasico() {
+		
+		int a=-1;
+		int b=6;
+		int c=2;
+		
+		if (a>=b) {
+			
+			System.out.println("a é maior ou igual a b");
+		}
+			else {
+				System.out.println("a não é maior que b");
+			}
+			
+			return;
+		}
+	
+	}
+
+
