7da6e02d77771e6c7eaa9956888e7b29c8aef7ee
etorres74 <edmilson@L-Debian>
2019-12-10 22:24:54 -0200

    Commit de hoje segundo dia.. no git

--- TestePrimeiroGit/src/minhasclasses/FirstClassGit.java
+++ TestePrimeiroGit/src/minhasclasses/FirstClassGit.java
@@ -4,7 +4,7 @@ public class FirstClassGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+   System.out.println("Este foi o primeiro projeto no Git, Agora eu estou melhorando!");
 	}
 
 }
