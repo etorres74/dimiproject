371503f5d4bc5eb46f921589eed918a58692951c
Edmilson <Edmilson@192.168.0.26>
2022-05-26 10:01:32 -0300

    Modificado classe principal
    Change-Id: Ifdfd2098a04df1aedf5f9805c6ee421c5e3f8b29
    Change-Id: 001

---
+++ TesteCompilacao/src/teste/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package teste;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+	System.out.println("MEU NOME É EDEMILLLLSÔ");
+	System.out.println("Ola de novo com Git");
+		
+	}
+	
+}
